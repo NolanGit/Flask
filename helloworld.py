2829afee708cefccfaff1fd1df896f615fa1946a
NolanSun <shr1213@live.com>
2019-01-18 17:46:29 +0800

    20190118

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,34 @@
 import os
+import time
+import datetime
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask import send_from_directory
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('demo.html')
+    spring = datetime.datetime(2019, 2, 5, 0, 0, 0)
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    today = datetime.datetime.now()
+    day = (spring - today).days
+    second = (spring - today).seconds
+    count_seconds = second % 60
+    count_minutes = second // 60 % 60
+    count_hours = second // 60 // 60
+    if count_hours > 24:
+        count_hours = count_hours - 24
+    templateData = {'time': current_time, 'count_days': day, 'count_hours': count_hours, 'count_minutes': count_minutes, 'count_seconds': count_seconds}
+    return render_template('demo.html', **templateData)
+
 
 @app.route('/favicon.ico')
 def favicon():
-        return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000) 
+    app.run(host='0.0.0.0', port=5000, debug=True)
