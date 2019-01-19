2bfdb5e4d8438e79baf9f85eb7132a0fdc148242
NolanSun <shr1213@live.com>
2019-01-19 19:16:27 +0800

    20190119

--- helloworld.py
+++ helloworld.py
@@ -25,6 +25,11 @@ def index():
     return render_template('demo.html', **templateData)
 
 
+@app.route('/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
