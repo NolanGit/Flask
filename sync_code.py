366684fdaba838b0cfd69e047f72488f94ac755c
NolanSun <shr1213@live.com>
2019-01-21 19:09:11 +0800

    20190121

--- sync_code.py
+++ sync_code.py
@@ -1,3 +1,7 @@
 # coding=utf-8
 import os
-import configparser
+from common.Config import ConfigReader
+
+config_reader = ConfigReader()
+JENKINS_WORKSPACE = config_reader.get_config('config', 'JENKINS_WORKSPACE')
+APP_LOCATION = config_reader.get_config('config', 'APP_LOCATION')
