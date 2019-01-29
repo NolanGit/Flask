3599c0ca9a665f6b5312a17334eab65240f06781
NolanSun <shr1213@live.com>
2019-01-29 17:37:03 +0800

    20190129

--- helloworld.py
+++ helloworld.py
@@ -2,27 +2,54 @@ import os
 import time
 import datetime
 from flask import Flask
-from flask import request
+from flask import request, redirect, url_for
 from flask_moment import Moment
+from flask_wtf import FlaskForm
 from flask import render_template
 from flask import send_from_directory
 from flask_bootstrap import Bootstrap
-
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+global name
+
+
+class NameForm(FlaskForm):
+    name = StringField('请输入您的名字，查看春节倒计时：', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    global name
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        return redirect(url_for('new_year'))
+    return render_template('index.html', form=form, name=name)
+
+
+@app.route('/new_year')
+def new_year():
+    global name
     spring_festival = datetime.datetime(2019, 2, 5, 0, 0, 0)
     current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     today = datetime.datetime.now()
@@ -31,12 +58,34 @@ def index():
     count_seconds = second % 60
     count_minutes = second // 60 % 60
     count_hours = second // 60 // 60
+    current_utctime = datetime.datetime.utcnow()
     if count_hours > 24:
         count_hours = count_hours - 24
-    templateData = {'time': current_time, 'count_days': day, 'count_hours': count_hours, 'count_minutes': count_minutes, 'count_seconds': count_seconds}
-    return render_template('demo.html', **templateData)
+    templateData = {
+        'name': name,
+        'time': current_time,
+        'count_days': day,
+        'count_hours': count_hours,
+        'count_minutes': count_minutes,
+        'count_seconds': count_seconds,
+        'current_utctime': current_utctime
+    }
+    return render_template('new_year.html', **templateData)
 
-
-
+@app.route('/tools')
+def tools():
+    for x in range(100):
+        current_ip = os.popen("curl icanhazip.com").read()
+        current_ip = str(current_ip.replace("\n", ""))
+        if current_ip != None and current_ip !='':
+            break
+        else:
+            print("WARNING: Current extranet IP is : " + current_ip)
+            raise Exception("Bad requests !")
+    print("Current extranet IP is : " + current_ip)
+    templateData = {
+        'ip': current_ip
+    }
+    return render_template('tools.html', **templateData)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
