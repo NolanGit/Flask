adebd06092ae0dc4bec571305f6548b9e1a9cef4
NolanSun <shr1213@live.com>
2019-03-15 21:10:52 +0800

    20190315

--- run.py
+++ run.py
@@ -19,4 +19,4 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6666, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
