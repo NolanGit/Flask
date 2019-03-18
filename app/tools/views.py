9e8d10a7c1a5251c666c2258b7c8b4d9c585a852
NolanSun <shr1213@live.com>
2019-03-18 10:42:54 +0800

    20190318

--- app/tools/views.py
+++ app/tools/views.py
@@ -29,6 +29,18 @@ def goldDetail():
     for price in gold_price:
         datas['data'].append({'date':price.date,'price':price.price})
     print(datas)
+    datas = {
+		"data":[
+			{"name":"allpe","num":100},
+			{"name":"peach","num":123},
+			{"name":"Pear","num":234},
+			{"name":"avocado","num":20},
+			{"name":"cantaloupe","num":1},
+			{"name":"Banana","num":77},
+			{"name":"Grape","num":43},
+			{"name":"apricot","num":0}
+		]
+	}
     content = json.dumps(datas)
     resp = Response_headers(content)
     return (resp)
